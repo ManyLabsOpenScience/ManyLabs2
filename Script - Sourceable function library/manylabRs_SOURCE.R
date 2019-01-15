4c5aa47a31c71f476995ca314ef2d599d00a8d63
Fred Hasselman <namlessah.derf@gmail.com>
2019-01-15 13:29:58 +0100

    Update decide.analysis()

--- Script - Sourceable function library/manylabRs_SOURCE.R
+++ Script - Sourceable function library/manylabRs_SOURCE.R
@@ -1819,7 +1819,7 @@ decide.analysis <- function(ML2.key, studies=NA, tp = NA, doAll = FALSE){
   }
 
   if(is.na(tp[1])){
-    disp(paste0("Analyzing global, primary and secondary studies"), header = FALSE, footer = TRUE)
+    disp(paste0("Analyzing global, primary and secondary studies marked '1' in the corrsponding columns of the masteRkey spreadsheet"), header = FALSE, footer = TRUE)
     studiess <- c(ML2.key$unique.id[ML2.key$study.global.include[studies]==1],
                   ML2.key$unique.id[ML2.key$study.primary.include[studies]==1],
                   ML2.key$unique.id[ML2.key$study.secondary.include[studies]==1])
@@ -1829,7 +1829,11 @@ decide.analysis <- function(ML2.key, studies=NA, tp = NA, doAll = FALSE){
     )
   } else {
     disp(paste0("Analyzing studies in ", analysis[tp]), header = FALSE, footer = TRUE)
-    studiess <- lapply(analysis[tp], function(c) ML2.key$unique.id[ML2.key[,c]==1])
+    if(doAll){
+      studiess <- lapply(analysis[tp], function(c) ML2.key$unique.id[ML2.key[,c]>=0])
+    } else {
+      studiess <- lapply(analysis[tp], function(c) ML2.key$unique.id[ML2.key[,c]==1])
+    }
     tps      <- unlist(sapply(seq_along(studiess), function(s) rep(tp[s], length(studiess[[s]]))))
     studiess <- unlist(studiess)
   }
